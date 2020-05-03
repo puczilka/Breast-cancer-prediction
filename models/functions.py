50507298694a57d8b337143e57a3de5901ea3a2f
puczilka <marta.monika.kwiatkowska@gmail.com>
2020-05-03 17:49:27 +0100

    Adding Learning curves for gridsearchCV
    Created a function to plot multiple evaluation metrics performance over crossvalidated grid search.

--- models/functions.py
+++ models/functions.py
@@ -16,6 +16,15 @@ import numpy as np
 from IPython.display import Math
 from itertools import product
 
+#### Import ML lib
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import make_scorer
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.tree import DecisionTreeClassifier
+
+
+
 def plot_confusion_matrix(cm, classes,
                           normalize=False,
                           axis=1,
@@ -74,3 +83,52 @@ def plot_confusion_matrix(cm, classes,
     plt.ylabel('True label')
     plt.xlabel('Predicted label')
     return None
+
+
+
+def plot_performance(results):
+    plt.figure(figsize=(13, 13))
+    plt.title("GridSearchCV evaluating using multiple scorers simultaneously", fontsize=16)
+
+    plt.xlabel("min_samples_split")
+    plt.ylabel("Score")
+
+    ax = plt.gca()
+    ax.set_xlim(1, 10)
+    ax.set_ylim(0.73, 1)
+    
+    scoring = {'Accuracy' : make_scorer(accuracy_score), 'Precision': make_scorer(precision_score)}
+
+    params = {
+    'max_features': [4,5],
+    'n_estimators': [5,10],
+    'max_depth': [1,10]
+    }
+
+# Get the regular numpy array from the MaskedArray
+    X_axis = np.array(results["param_max_depth"], dtype=float)
+
+    for scorer, color in zip(sorted(scoring), ['g', 'k']):
+        for sample, style in (('train', '--'), ('test', '-')):
+            sample_score_mean = results['mean_%s_%s' % (sample, scorer)]
+            sample_score_std = results['std_%s_%s' % (sample, scorer)]
+            ax.fill_between(X_axis, sample_score_mean - sample_score_std, sample_score_mean + sample_score_std,
+            alpha=0.1 if sample == 'test' else 0, color=color)
+            ax.plot(X_axis, sample_score_mean, style, color=color,
+            alpha=1 if sample == 'test' else 0.7,
+            label="%s (%s)" % (scorer, sample))
+
+        best_index = np.nonzero(results['rank_test_%s' % scorer] == 1)[0][0]
+        best_score = results['mean_test_%s' % scorer][best_index]
+
+        # Plot a dotted vertical line at the best score for that scorer marked by x
+        ax.plot([X_axis[best_index], ] * 2, [0, best_score], linestyle='-.', color=color, marker='x', markeredgewidth=3, ms=8)
+
+    # Annotate the best score for that scorer
+        ax.annotate("%0.2f" % best_score,(X_axis[best_index], best_score + 0.005))
+
+    plt.legend(loc="best")
+    plt.grid(False)
+    plt.show()
+    return None
+
